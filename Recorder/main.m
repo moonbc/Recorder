d4b0ee0e6ad4d121bd8255b4d0b78ee7bebed154
Ben <ben@mykoon.com>
2018-11-26 10:59:35 +0900

    Initial Commit

---
+++ Recorder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Recorder
+//
+//  Created by ByungChen Moon on 26/11/2018.
+//  Copyright © 2018 ByungChen Moon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
